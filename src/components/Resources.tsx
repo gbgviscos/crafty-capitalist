9e128fe7e30449c8994038afee81c465b0154740
gbgviscos <Viscos@gmail.com>
2023-09-20 16:38:04 +0200

    fixed a type error

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+interface Resource {
+  name: string;
+  amount: number;
+}
+
 interface ResourcesProps {
-  resources: Record<string, number>;
+  resources: Record<string, Resource>;
 }
 
 const Resources: React.FC<ResourcesProps> = ({ resources }) => {
@@ -9,10 +14,10 @@ const Resources: React.FC<ResourcesProps> = ({ resources }) => {
     <div className="border p-4 mb-4">
       <h2 className="text-xl mb-2">Resources</h2>
       <ul>
-        {Object.entries(resources).map(([resource, count]) => (
-          <li key={resource} className="flex justify-between mb-2">
+        {Object.entries(resources).map(([resourceName, resource]) => (
+          <li key={resourceName} className="flex justify-between mb-2">
             <span>{resource.name}:</span>
-            <span>{count}</span>
+            <span>{resource.amount}</span>
           </li>
         ))}
       </ul>
